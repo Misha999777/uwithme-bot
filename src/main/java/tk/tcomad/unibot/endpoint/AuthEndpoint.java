f12caed9ae650117c362350c4357a15615c48682
Михаил Грошевой <43928021+Misha999777@users.noreply.github.com>
2021-11-13 22:38:40 +0200

    Auth improvements

--- src/main/java/tk/tcomad/unibot/endpoint/AuthEndpoint.java
+++ src/main/java/tk/tcomad/unibot/endpoint/AuthEndpoint.java
@@ -139,6 +139,7 @@ public class AuthEndpoint {
                       .concat("/token")
                       .concat("&response_type=code")
                       .concat("&scope=openid")
+                      .concat("&prompt=login")
                       .concat("&state=")
                       .concat(state);
     }

2ee9149685819557eba08238dbbd250893cfceed
Mikhail Groshevoy <developer.misha999777@gmail.com>
2021-11-15 21:03:42 +0200

    Auth improvements

--- src/main/java/tk/tcomad/unibot/endpoint/AuthEndpoint.java
+++ src/main/java/tk/tcomad/unibot/endpoint/AuthEndpoint.java
@@ -1,27 +1,23 @@
 package tk.tcomad.unibot.endpoint;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
+import static org.keycloak.OAuth2Constants.AUTHORIZATION_CODE;
+import static org.springframework.cloud.openfeign.security.OAuth2FeignRequestInterceptor.BEARER;
+import static org.springframework.http.HttpHeaders.LOCATION;
+
 import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
 
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import org.keycloak.KeycloakSecurityContext;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
@@ -33,25 +29,17 @@ import tk.tcomad.unibot.dto.keycloak.TokenResponse;
 import tk.tcomad.unibot.dto.keycloak.UserSession;
 import tk.tcomad.unibot.repository.BotUserRepository;
 import tk.tcomad.unibot.telegram.TelegramBot;
+import tk.tcomad.unibot.util.SHA256Utility;
+import tk.tcomad.unibot.util.StringUtility;
 
 @Controller
 @RequiredArgsConstructor
 public class AuthEndpoint {
 
-    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
-
-    @Value("${spring.security.oauth2.client.provider.keycloak.authorization-uri}")
-    private String authUri;
-    @Value("${user.logout.uri}")
-    private String logoutUri;
     @Value("${keycloak.resource}")
     private String client;
     @Value("${resource.id}")
     private String clientId;
-    @Value("${user.login.redirect.uri}")
-    private String redirectUri;
-    @Value("${bot.key}")
-    private String key;
     @Value("${keycloak.credentials.secret}")
     private String clientSecret;
     @Value("${education.app.frontend.uri}")
@@ -62,17 +50,19 @@ public class AuthEndpoint {
     private final KeycloakClient keycloakClient;
     private final EducationAppClient studentsClient;
     private final KeycloakAdminClient keycloakAdminClient;
+    private final StringUtility stringUtility;
+    private final SHA256Utility sha256Utility;
 
     @GetMapping
     public void redirect(HttpServletResponse httpServletResponse) {
-        httpServletResponse.setHeader("Location", frontendUri);
+        httpServletResponse.setHeader(LOCATION, frontendUri);
         httpServletResponse.setStatus(302);
     }
 
     @GetMapping("/login")
     @Transactional
     public void login(HttpServletResponse httpServletResponse, @RequestParam Map<String, String> request) {
-        httpServletResponse.setHeader("Location", constructLoginUri(request));
+        httpServletResponse.setHeader(LOCATION, stringUtility.constructLoginUri(request));
         httpServletResponse.setStatus(302);
     }
 
@@ -80,18 +70,14 @@ public class AuthEndpoint {
     public String token(Model model, @RequestParam Map<String, String> request) {
         var tokenResponse = getTokenResponseAndInvalidateDedicatedSessions(request);
 
-        checkAuth(request);
+        sha256Utility.checkTelegramData(request);
 
         var botUser = botUserRepository.findById(Long.parseLong(request.get("id"))).orElseThrow();
         botUser.setUserId(tokenResponse.getUserId());
         botUser.setRefreshToken(tokenResponse.getRefresh_token());
         botUserRepository.save(botUser);
 
-        model.addAttribute("username", request.get("username"));
-        model.addAttribute("photo_url", request.get("photo_url"));
-        model.addAttribute("token", tokenResponse.getAccess_token());
-        model.addAttribute("userLink", "https://t.me/" + request.get("username"));
-        model.addAttribute("logoutUri", constructLogoutUri());
+        stringUtility.constructModel(model, request, tokenResponse.getAccess_token());
         return "login.html";
     }
 
@@ -127,49 +113,17 @@ public class AuthEndpoint {
         return modelAndView;
     }
 
-    private String constructRedirectUri(Map<String, String> request) {
-        return redirectUri.concat("/token")
-                          .concat("?id=")
-                          .concat(request.get("id"))
-                          .concat("&first_name=")
-                          .concat(request.get("first_name"))
-                          .concat("&last_name=")
-                          .concat(request.get("last_name"))
-                          .concat("&username=")
-                          .concat(request.get("username"))
-                          .concat("&photo_url=")
-                          .concat(request.get("photo_url"))
-                          .concat("&auth_date=")
-                          .concat(request.get("auth_date"))
-                          .concat("&hash=")
-                          .concat(request.get("hash"));
-    }
-
-    private String constructLoginUri(Map<String, String> request) {
-        return authUri.concat("?client_id=")
-                      .concat(client)
-                      .concat("&redirect_uri=")
-                      .concat(URLEncoder.encode(constructRedirectUri(request), StandardCharsets.UTF_8))
-                      .concat("&response_type=code")
-                      .concat("&scope=openid");
-    }
-
-    private String constructLogoutUri() {
-        return logoutUri.concat("?redirect_uri=")
-                        .concat(redirectUri)
-                        .concat("/close");
-    }
-
     private TokenResponse getTokenResponseAndInvalidateDedicatedSessions(Map<String, String> request) {
-        var redirect = constructRedirectUri(request);
+        var redirect = stringUtility.constructRedirectUri(request);
         var code = request.get("code");
 
         var token = keycloakClient.getToken(new AuthTokenRequest(code,
                                                                  redirect,
                                                                  client,
-                                                                 "authorization_code",
+                                                                 AUTHORIZATION_CODE,
                                                                  clientSecret));
-        var userId = keycloakClient.getUserInfo("Bearer " + token.getAccess_token()).getSub();
+        var userId = keycloakClient.getUserInfo(BEARER + " " + token.getAccess_token()).getSub();
+
         keycloakAdminClient.getUserSessions(clientId).stream()
                            .filter(userSession -> userSession.getUserId().equals(userId))
                            .filter(userSession -> userSession.getClients().containsValue(client))
@@ -179,39 +133,5 @@ public class AuthEndpoint {
 
         token.setUserId(userId);
         return token;
-    }
-
-    @SneakyThrows
-    private void checkAuth(@RequestBody Map<String, String> request) {
-        request.remove("code");
-        String hash = request.get("hash");
-        request.remove("hash");
-
-        String str = request.entrySet().stream()
-                            .sorted((a, b) -> a.getKey().compareToIgnoreCase(b.getKey()))
-                            .map(kvp -> kvp.getKey() + "=" + kvp.getValue())
-                            .collect(Collectors.joining("\n"));
-
-        var spec = new SecretKeySpec(MessageDigest.getInstance("SHA-256").digest(key.getBytes(StandardCharsets.UTF_8)),
-                                     "HmacSHA256");
-        Mac mac = Mac.getInstance("HmacSHA256");
-        mac.init(spec);
-
-        var result = mac.doFinal(str.getBytes(StandardCharsets.UTF_8));
-        String resultStr = bytesToHex(result);
-
-        if (hash.compareToIgnoreCase(resultStr) != 0) {
-            throw new RuntimeException();
-        }
-    }
-
-    private static String bytesToHex(byte[] bytes) {
-        var hexChars = new char[bytes.length * 2];
-        for (var j = 0; j < bytes.length; j++) {
-            var v = bytes[j] & 0xFF;
-            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
-        }
-        return new String(hexChars);
     }
 }
---
+++ src/main/java/tk/tcomad/unibot/util/StringUtility.java
@@ -0,0 +1,91 @@
+package tk.tcomad.unibot.util;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.StringJoiner;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+
+@Component
+public class StringUtility {
+
+    private static final String ID = "id";
+    private static final String FIRST_NAME = "first_name";
+    private static final String LAST_NAME = "last_name";
+    private static final String USERNAME = "username";
+    private static final String PHOTO_URL = "photo_url";
+    private static final String AUTH_DATE = "auth_date";
+    private static final String HASH = "hash";
+    private static final String TOKEN_PATH = "/token";
+    private static final String CLIENT_ID = "client_id";
+    private static final String REDIRECT_URI = "redirect_uri";
+    private static final String RESPONSE_TYPE = "response_type";
+    private static final String CODE = "code";
+    private static final String SCOPE = "scope";
+    private static final String OPENID = "openid";
+    private static final String CLOSE_PATH = "/close";
+    private static final String T_ME = "https://t.me/";
+    private static final String USER_URL = "user_url";
+    private static final String LOGOUT_URL = "logout_url";
+    private static final String TOKEN = "token";
+
+    @Value("${spring.security.oauth2.client.provider.keycloak.authorization-uri}")
+    private String authUri;
+    @Value("${user.logout.uri}")
+    private String logoutUri;
+    @Value("${user.login.redirect.uri}")
+    private String redirectUri;
+    @Value("${keycloak.resource}")
+    private String client;
+
+    public String constructRedirectUri(Map<String, String> request) {
+        var params = new StringJoiner("&", "?", "")
+                .add(constructParam(ID, request.get(ID)))
+                .add(constructParam(FIRST_NAME, request.get(FIRST_NAME)))
+                .add(constructParam(LAST_NAME, request.get(LAST_NAME)))
+                .add(constructParam(USERNAME, request.get(USERNAME)))
+                .add(constructParam(PHOTO_URL, request.get(PHOTO_URL)))
+                .add(constructParam(AUTH_DATE, request.get(AUTH_DATE)))
+                .add(constructParam(HASH, request.get(HASH)))
+                .toString();
+
+        return redirectUri.concat(TOKEN_PATH)
+                          .concat(params);
+    }
+
+    public String constructLoginUri(Map<String, String> request) {
+        var params = new StringJoiner("&", "?", "")
+                .add(constructParam(CLIENT_ID, client))
+                .add(constructParam(REDIRECT_URI, URLEncoder.encode(constructRedirectUri(request),
+                                                                    StandardCharsets.UTF_8)))
+                .add(constructParam(RESPONSE_TYPE, CODE))
+                .add(constructParam(SCOPE, OPENID))
+                .toString();
+
+        return authUri.concat(params);
+    }
+
+    public String constructLogoutUri() {
+        var params = new StringJoiner("&", "?", "")
+                .add(constructParam(REDIRECT_URI, redirectUri + CLOSE_PATH))
+                .toString();
+
+        return logoutUri.concat(params);
+    }
+
+    public void constructModel(Model model, Map<String, String> request, String accessToken) {
+        model.addAttribute(USERNAME, request.get(USERNAME));
+        model.addAttribute(PHOTO_URL, request.get(PHOTO_URL));
+        model.addAttribute(TOKEN, accessToken);
+        model.addAttribute(USER_URL, T_ME + request.get(USERNAME));
+        model.addAttribute(LOGOUT_URL, constructLogoutUri());
+    }
+
+    private String constructParam(String name, String value) {
+        return name.concat("=")
+                   .concat(value);
+    }
+}
